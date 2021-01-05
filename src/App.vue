0229169d99a5e78f821bbb3620e59a9e248ae0be
lirongyue <761780862@qq.com>
2021-01-05 23:28:55 +0800

    vue-router出初始化

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <div>你好</div>
-  <Frank/>
+  <div>
+    导航栏
+    |
+    <router-link to="/">Frank</router-link>
+    |
+    <router-link to="/frank2">Frank2</router-link>
+  </div>
+
+  <hr>
+  <router-view/>
 </template>
 
 <script>
-  import Frank from './components/Frank.vue'
 
   export default {
     name: 'App',
-    components: {
-      Frank
-    }
   }
 </script>
