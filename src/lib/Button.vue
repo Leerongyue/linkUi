2e09d0205f9385a695f844879041b33564154fcd
lirongyue <761780862@qq.com>
2021-01-18 23:35:03 +0800

    Buttonç支持level属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -16,14 +16,19 @@
       size: {
         type: String,
         default: 'normal'
+      },
+      level: {
+        type: String,
+        default: 'normal'
       }
     },
     setup(props) {
-      const {theme, size} = props;
+      const {theme, size, level} = props;
       const classes = computed(() => {
         return {
           [`link-theme-${theme}`]: theme,
-          [`link-size-${size}`]: size
+          [`link-size-${size}`]: size,
+          [`link-level-${level}`]: level
         };
       });
       return {classes};
@@ -37,6 +42,7 @@
   $color: #333;
   $blue: #40a9ff;
   $radius: 4px;
+  $red: red;
 
   .link-button {
     padding: 0 12px;
@@ -99,6 +105,64 @@
       font-size: 12px;
       height: 20px;
       padding: 0 4px;
+    }
+
+    &.link-theme-button {
+
+      &.link-level-main {
+        background: $blue;
+        color: white;
+        border-color: $blue;
+
+        &:hover,
+        &:focus {
+          background: darken($blue, 10%);
+          border-color: darken($blue, 10%);
+        }
+      }
+
+      &.link-level-danger {
+        background: $red;
+        border-color: $red;
+        color: white;
+
+        &:hover,
+        &:focus {
+          background: darken($red, 10%);
+          border-color: darken($red, 10%);
+        }
+      }
+    }
+
+    &.link-theme-link {
+      &.link-level-danger {
+        color: $red;
+
+        &:hover,
+        &:focus {
+          color: darken($red, 10%);
+        }
+      }
+    }
+
+    &.link-theme-text {
+      &.link-level-main {
+        color: $blue;
+
+        &:hover,
+        &:focus {
+          color: darken($blue, 10%);
+        }
+      }
+
+      &.link-level-danger {
+        color: $red;
+
+        &:hover,
+        &:focus {
+          color: darken($red, 10%);
+        }
+      }
     }
   }
 
