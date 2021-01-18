eaca133745bd84b8e3604439ccb4e55dfbf17888
Administrator <761780862@qq.com>
2021-01-18 13:18:51 +0800

    butotn base style

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -6,7 +6,6 @@
 
 <script lang="ts">
   export default {
-    // inheritAttrs: false,
     props: {
       theme: {
         type: String,
@@ -20,8 +19,44 @@
 </script>
 
 <style lang="scss" scoped>
-  button {
-    background: gray;
-    color: white;
+  $h: 32px;
+  $border-color: #d9d9d9;
+  $color: #333;
+  $blue: #40a9ff;
+  $radius: 4px;
+
+  .link-button {
+    padding: 0 12px;
+    /*cursor: pointer;*/
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    white-space: nowrap;
+    background: white;
+    color: $color;
+    border: 1px solid $border-color;
+    border-radius: $radius;
+    box-shadow: 0 1px 0 fade-out(black, 0.95);
+
+    & + & {
+      margin-left: 8px;
+    }
+
+    &:hover, &:focus {
+      color: $blue;
+      border-color: $blue;
+    }
+
+    &:focus {
+      outline: none;
+    }
+
+    //shi pei firefox
+    &::-moz-focus-inner {
+      border: 0;
+    }
+
   }
+
+
 </style>
