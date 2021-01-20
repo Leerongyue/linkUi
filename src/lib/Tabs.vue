47ba56589136530ddc8ab89d55dd10487b9cb509
lirongyue <761780862@qq.com>
2021-01-20 22:59:19 +0800

    Tabs组件css1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    Tabs组件
+  <div class="link-tabs">
+    <div class="link-tabs-nav">
+      <div class="link-tabs-nav-item" v-for="(item,index) in titles" :key="index">{{item}}</div>
+    </div>
+    <div class="link-tabs-content">
+      <component class="link-tabs-content-item" v-for="(c,index) in defaults" :key="index" :is="c"></component>
+    </div>
   </div>
 </template>
 
@@ -17,11 +22,39 @@
           throw new Error('Tabs子标签必须是Tab');
         }
       });
-      return {defaults};
+      const titles = defaults.map(tag => tag.props.title);
+      return {defaults, titles};
     }
   };
 </script>
 
 <style lang="scss" scoped>
+  $blue: #40a9ff;
+  $color: #333;
+  $border-color: #d9d9d9;
+  .link-tabs {
+    &-nav {
+      display: flex;
+      color: $color;
+      border-bottom: 1px solid $border-color;
 
+      &-item {
+        padding: 8px 0;
+        margin: 0 16px;
+        cursor: pointer;
+
+        &:first-child {
+          margin-left: 0;
+        }
+
+        &.selected {
+          color: $blue;
+        }
+      }
+    }
+
+    &-content {
+      padding: 8px 0;
+    }
+  }
 </style>
